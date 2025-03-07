f51b3649a709766fb255b733f5749c193121ed31
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2025-03-07 16:01:03 -0500

    creo la clase TermRepository

--- src/modules/glossary/repository/term.repository.ts
+++ src/modules/glossary/repository/term.repository.ts
@@ -1,11 +1,27 @@
-import { Injectable } from "@nestjs/common";
+
+import { Injectable} from "@nestjs/common";
 /**
- * con este decorador puedo inyectar la clase en otros servicios
+ *  Es un decorador de NestJS que marca esta clase como un servicio que puede ser inyectado en otras clases.
  */
 import { InjectRepository } from "@nestjs/typeorm";
 /**
  * El decorador Me da la posibilidad de inyectar el repositorio para poder trabajar con el en la clase
  */
+import { Repository } from "typeorm";
+/**
+ * Me permite realizar operaciones basicas como crear,leer,actualizar y eliminar
+ */
 import { Terms } from "../entities/term.entity";
 
+@Injectable()
+export class TermRespository{
+    constructor(
+        @InjectRepository(Terms) // inyecto el repositorio 
+        private termRepository: Repository<Terms> // Propiedad privada llammada termRepository que es un Repositorio para trabajar específicamente
+        //  con la entidad Terms
+    ){}
 
+    
+
+
+}
